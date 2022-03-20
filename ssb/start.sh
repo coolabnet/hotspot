38e6c05dc85f037048a6dc34c1fa1a35c25e9a54
luandro <luandro@gmail.com>
2022-03-20 07:24:04 -0300

    feat: go-ssb working on arm, broadcasting on localnetwork, renaming itself and being promiscous

--- ssb/start.sh
+++ ssb/start.sh
@@ -1,32 +1,21 @@
 #!/usr/bin/env bash
 
 # ssb
-echo 'Cleaning up' # Necessary: https://github.com/cryptoscope/ssb/issues/124
-rm -rf /var/lib/ssb-pub/sublogs/shared-badger/
-# ssb-pub-go -repo /var/lib/ssb-pub -fsck sequences -repair
 echo 'Starting ssb pub'
-ssb-pub &
+echo 'Cleaning up' # Necessary: https://github.com/cryptoscope/ssb/issues/124
+rm -rf /root/.ssb-go/sublogs/shared-badger/
+go-sbot &
 sleep 10
 echo 'POSTing ssb invite'
-ssb-cli invite create --uses 9999 > /var/www/html/invite.json
+sbotcli invite create --uses 9999 > /var/www/html/invite.json
+ID=($(sbotcli call whoami  | jq '.id' | tr -d '"'))
+echo $ID
+sbotcli publish about --name "P2P Pub" $ID
+# Connect to room
+# sbotcli call conn.replicate '@uMYDVPuEKftL4SzpRGVyQxLdyPkOiX7njit7+qT/7IQ=.ed25519'
+# sbotcli call conn.connect 'net:hermies.club:8008~shs:uMYDVPuEKftL4SzpRGVyQxLdyPkOiX7njit7+qT/7IQ='
 
 # apache
 source /etc/apache2/envvars
-mkdir /var/run/apache2
 /usr/sbin/apache2
-
-# mdns
-set -m
-
-GREEN='\033[0;32m'
-echo -e "${GREEN}Systemd init system enabled."
-
-# systemd causes a POLLHUP for console FD to occur
-# on startup once all other process have stopped.
-# We need this sleep to ensure this doesn't occur, else
-# logging to the console will not work.
-sleep infinity &
-for var in $(compgen -e); do
-	printf '%q=%q\n' "$var" "${!var}"
-done > /etc/docker.env
-exec /lib/systemd/systemd
+sleep infinity
